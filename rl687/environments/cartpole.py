c3564c824ddbdd0a0e4aa841a53167b4ba13ec06
Purujit Goyal <purujit@1x-nat-vl930-172-30-72-131.wireless.umass.edu>
2019-09-29 17:14:00 -0400

    implemented cartpole env and tabular softmax policy

--- rl687/environments/cartpole.py
+++ rl687/environments/cartpole.py
@@ -18,12 +18,11 @@ class Cartpole(Environment):
     """
 
     def __init__(self):
-        # TODO: properly define the variables below
-        self._name = "???"
-        self._action = 0
-        self._reward = 0
-        self._isEnd = 0
-        self._gamma = 0.0
+        self._name = "Cartpole"
+        self._action = None
+        self._reward = 0.
+        self._isEnd = False
+        self._gamma = 1.
 
         # define the state # NOTE: you must use these variable names
         self._x = 0.  # horizontal position of cart
@@ -32,67 +31,96 @@ class Cartpole(Environment):
         self._dtheta = 0.  # angular velocity of the pole
 
         # dynamics
-        self._g = 0.0  # gravitational acceleration (m/s^2)
-        self._mp = 0.0  # pole mass
-        self._mc = 0.0  # cart mass
-        self._l = 0.0  # (1/2) * pole length
-        self._dt = 0.0  # timestep
+        self._g = 9.8  # gravitational acceleration (m/s^2)
+        self._mp = 0.1  # pole mass
+        self._mc = 1.0  # cart mass
+        self._l = 0.5  # (1/2) * pole length
+        self._dt = 0.02  # timestep
         self._t = 0.0  # total time elapsed  NOTE: USE must use this variable
+        self._maxF = 10.0  # max force on cart
+        self._failAngle = np.pi/12.0
 
     @property
     def name(self) -> str:
-        # TODO
-        pass
+        return self._name
 
     @property
     def reward(self) -> float:
-        # TODO
-        pass
+        return self._reward
 
     @property
     def gamma(self) -> float:
-        # TODO
-        pass
+        return self._gamma
 
     @property
     def action(self) -> int:
-        # TODO
-        pass
+        return self._action
 
     @property
     def isEnd(self) -> bool:
-        # TODO
-        pass
+        return self._isEnd
 
     @property
     def state(self) -> np.ndarray:
-        # TODO
-        pass
+        return np.array([self._x, self._v, self._theta, self._dtheta])
 
     def nextState(self, state: np.ndarray, action: int) -> np.ndarray:
         """
         Compute the next state of the pendulum using the euler approximation to the dynamics
         """
-        # TODO
-        pass
+        if self.isEnd:
+            return state
+
+        force = self._maxF
+        if action == 0:
+            force *= -1
+
+        x_dot = state[1]
+        theta_dot = state[3]
+
+        cos_theta = np.cos(self._theta)
+        sin_theta = np.cos(self._theta)
+        total_mass = self._mp + self._mc
+
+        temp1 = force + (self._mp*self._l*(self._dtheta**2)*sin_theta/total_mass)
+        temp2 = 4.0/3.0 - (self._mp*(cos_theta**2)/total_mass)
+
+        dtheta_dot = (self._g*sin_theta - cos_theta*temp1)/(self._l*temp2)
+        v_dot = (force + self._mp*self._l*((theta_dot**2)*sin_theta - dtheta_dot*cos_theta))/total_mass
+
+        next_state = state + self._dt*np.array([x_dot, v_dot, theta_dot, dtheta_dot])
+        return next_state
 
     def R(self, state: np.ndarray, action: int, nextState: np.ndarray) -> float:
-        # TODO
-        pass
+        return 0 if (np.all(state == nextState) and self.isEnd) else 1
 
     def step(self, action: int) -> Tuple[np.ndarray, float, bool]:
         """
         takes one step in the environment and returns the next state, reward, and if it is in the terminal state
         """
-        # TODO
-        pass
+        next_state = self.nextState(self.state, action)
+        self._reward = self.R(self.state, action, next_state)
+        self._t += self._dt
+        self._x = next_state[0]
+        self._v = next_state[1]
+        self._theta = next_state[2]
+        self._dtheta = next_state[3]
+        self._isEnd = self.terminal()
+
+        return self.state, self.reward, self.isEnd
 
     def reset(self) -> None:
         """
         resets the state of the environment to the initial configuration
         """
-        # TODO
-        pass
+        self._x = 0.
+        self._v = 0.
+        self._theta = 0.
+        self._dtheta = 0.
+        self._reward = 0.
+        self._t = 0.
+        self._isEnd = False
+        self._action = None
 
     def terminal(self) -> bool:
         """
@@ -101,5 +129,7 @@ class Cartpole(Environment):
             pole falls |theta| > (pi/12.0)
             cart hits the sides |x| >= 3
         """
-        # TODO
-        pass
+        if (np.abs(self._theta) > self._failAngle) or np.abs(self._x) >= 3 or self._t > 20:
+            return True
+
+        return False
